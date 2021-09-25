dde0381fe29a3df2c59adc7b2686860668a5f1c5
xnaud <tmdtp3143@gmail.com>
2021-09-26 00:13:54 +0900

    0926

---
+++ 수치연산함수.sql
@@ -0,0 +1,16 @@
+-- 절대값
+SELECT ABS(-100)
+
+-- 자리수 올림
+SELECT ROUND(1234.56768,2)  -- 1234.5700
+SELECT ROUND(1234.5678,-2)  -- 1200.0000
+
+-- RANDOM값
+SELECT RAND()
+
+-- 제곱근값
+SELECT SQRT(4)  -- 2
+
+-- 자승 
+SELECT POWER(3,2)  -- 9
+
